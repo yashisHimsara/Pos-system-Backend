907ff718238ef6863fb614df1126da5cddfcf16f
yashisHimsara <139766123+yashisHimsara@users.noreply.github.com>
2024-08-27 23:28:06 +0530

    create BOFactory.java

--- src/main/java/lk/ijse/bo/BOFactory.java
+++ src/main/java/lk/ijse/bo/BOFactory.java
@@ -1,4 +1,30 @@
 package lk.ijse.bo;
 
 public class BOFactory {
+    public static BOFactory boFactory;
+
+    private BOFactory() {}
+
+    public static BOFactory getBoFactory(){
+        return (boFactory==null) ? boFactory=new BOFactory() : boFactory ;
+    }
+
+    public enum BOType {
+        CUSTOMER,ITEM,ORDER,USER;
+    }
+
+    public SuperBO getBO(BOType boType){
+        switch (boType){
+            case CUSTOMER:
+                return new CustomerBOImpl();
+            case ITEM:
+                return new ItemBOImpl();
+            case ORDER:
+                return new OrderBOImpl();
+            case USER:
+                return new UserBOImpl();
+            default:
+                return null;
+        }
+    }
 }
