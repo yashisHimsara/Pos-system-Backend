a883dd2afbf69ffd6a564a6376bf6483b060e685
yashisHimsara <139766123+yashisHimsara@users.noreply.github.com>
2024-08-28 14:57:13 +0530

    Update OrderBO.java

--- src/main/java/lk/ijse/bo/custom/OrderBO.java
+++ src/main/java/lk/ijse/bo/custom/OrderBO.java
@@ -1,4 +1,10 @@
 package lk.ijse.bo.custom;
 
-public interface OrderBO {
+import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.OrderDTO;
+
+import java.sql.Connection;
+
+public interface OrderBO extends SuperBO {
+    boolean saveOrder(OrderDTO orderDTO, Connection connection);
 }
