d325667c6ec88f62ff387fd247882efcb138e236
yashisHimsara <139766123+yashisHimsara@users.noreply.github.com>
2024-08-28 14:20:24 +0530

    Update ItemBO.java

--- src/main/java/lk/ijse/bo/custom/ItemBO.java
+++ src/main/java/lk/ijse/bo/custom/ItemBO.java
@@ -1,4 +1,16 @@
 package lk.ijse.bo.custom;
 
-public interface ItemBO {
+import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.ItemDTO;
+
+import java.sql.Connection;
+import java.util.List;
+
+public interface ItemBO extends SuperBO {
+    boolean saveItem(ItemDTO itemDTO, Connection connection);
+    List<ItemDTO> getAllItem(Connection connection);
+    boolean updateItem(ItemDTO itemDTO, String Id, Connection connection);
+    boolean isExistsItem(String id,Connection connection);
+    boolean deleteItem(String id,Connection connection);
+    ItemDTO get(String id,Connection connection);
 }
